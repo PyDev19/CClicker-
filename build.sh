b2f773f01d2d74c3715e4df036c609cfccf8972b
PyDev19 <atharvamishra956+1@gmail.com>
2023-03-19 17:29:48 +0530

    added comments and variables to make future additions easier

--- build.sh
+++ build.sh
@@ -1,7 +1,23 @@
-g++ -Isrc/classes -c src/classes/clicker.cpp -o build/compiled_files/clicker.o
-g++ -Isrc/classes -c src/classes/key.cpp -o build/compiled_files/key.o
+# Set variables for directories and files
+SOURCE_DIR=src
+CLASS_DIR=$SOURCE_DIR/classes
+BUILD_DIR=build
+COMPILED_DIR=$BUILD_DIR/compiled_files
+APP_DIR=$BUILD_DIR/app
 
-g++ -Isrc -c src/prompts.cpp -o build/compiled_files/prompts.o
-g++ -c src/main.cpp -o build/compiled_files/main.o
+# Compile the cpp files in classes folder
+echo 'Compiling c++ files in ' $CLASS_DIR ' directory...'
+g++ -I$CLASS_DIR -c $CLASS_DIR/clicker.cpp -o $COMPILED_DIR/clicker.o
+g++ -I$CLASS_DIR -c $CLASS_DIR/key.cpp -o $COMPILED_DIR/key.o
+echo 'Done'
 
-g++ build/compiled_files/*.o icon.res -o build/app/CClicker++.exe
+# Compile the cpp files in src folder
+echo 'Compiling c++ files in ' $SOURCE_DIR ' directory...'
+g++ -I$SOURCE_DIR -c $SOURCE_DIR/main.cpp -o $COMPILED_DIR/main.o
+g++ -c $SOURCE_DIR/prompts.cpp -o $COMPILED_DIR/prompts.o
+echo 'Done'
+
+# Link all object files and resource file
+echo 'Linking all object and resource files...'
+g++ $COMPILED_DIR/*.o icon.res -o $APP_DIR/CClicker++.exe
+echo 'Done'
