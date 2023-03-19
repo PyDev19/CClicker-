26d3ba5d0ae5b02b275181c5cd707f39029cc8ce
PyDev19 <atharvamishra956+1@gmail.com>
2023-03-19 17:15:02 +0530

    changed the name of final build exe.

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ g++ -Isrc/classes -c src/classes/key.cpp -o build/compiled_files/key.o
 g++ -Isrc -c src/prompts.cpp -o build/compiled_files/prompts.o
 g++ -c src/main.cpp -o build/compiled_files/main.o
 
-g++ build/compiled_files/*.o icon.res -o build/app/main.exe
+g++ build/compiled_files/*.o icon.res -o build/app/CClicker++.exe
