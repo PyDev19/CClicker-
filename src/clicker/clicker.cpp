e8d01bc4264f19809e6bff7d050b862518d36aef
PyDev19 <atharvamishra956+1@gmail.com>
2023-03-20 14:48:18 +0530

    implemented color print messages and switch statements

--- src/clicker/clicker.cpp
+++ src/clicker/clicker.cpp
@@ -3,6 +3,7 @@
 #include <tuple>
 
 #include "clicker.hpp"
+#include "../colors/colors.hpp"
 
 Clicker::Clicker() {
     started = false;
@@ -19,16 +20,18 @@ Clicker::Clicker() {
 
 void Clicker::mouse_clicker(char start_key, char stop_key, char exit_key, int button, float delay) {
     input.type = INPUT_MOUSE;
+    Colors* color = new Colors;
 
     while (true) {
         if (printed_1 != true) {
-            std::cout << "Autoclicker Running" << std::endl;
+            color->color_print("Autoclicker Running", color->PREFERRED_PROCESS_ENDING_COLOR, "");
+            std::cout << "\n" << std::endl;
             printed_1 = true;
         }
 
         if (GetAsyncKeyState(start_key) & 0x8000 && started == false) {
             if (printed_2 != true) {
-                std::cout << "Autoclicker Started" << std::endl;
+                color->color_print("Autoclicker Started", color->PREFERRED_PROCESS_STARTING_COLOR, "");
                 printed_2 = true;
             }
             printed_3 = false;
@@ -37,7 +40,7 @@ void Clicker::mouse_clicker(char start_key, char stop_key, char exit_key, int bu
             started = true;
 		} else if (GetAsyncKeyState(stop_key) & 0x8000 && started == true) {
             if (printed_3 != true) {
-                std::cout << "Autoclicker Stopped" << std::endl;
+                color->color_print("Autoclicker Stopped", color->PREFERRED_PROCESS_ENDING_COLOR, "");
                 printed_3 = true;
             }
             printed_2 = false;
@@ -45,35 +48,41 @@ void Clicker::mouse_clicker(char start_key, char stop_key, char exit_key, int bu
             click = false;
             started = false;
 		} else if (GetAsyncKeyState(exit_key) & 0x8000) {
-            std::cout << "Exiting Program" << std::endl;
+            color->color_print("Exiting Program", color->PREFERRED_ERROR_COLOR, "");
             click = false;
             break;
         }
 
 		if (click == true) {
-            if (button == 1) {
-                input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
-            
-            } else if (button == 2) {
-                input.mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
-            
-            } else if (button == 4) {
-                input.mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN;
+            switch (button) {
+                case 1:
+                    input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+                    break;
+                case 2:
+                    input.mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
+                    break;
+                case 4:
+                    input.mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN;
+                    break;
             }
 			SendInput(1, &input, sizeof(INPUT));
 
-            if (button == 1) {
-                input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
-            
-            } else if (button == 2) {
-                input.mi.dwFlags = MOUSEEVENTF_RIGHTUP;
-            
-            } else if (button == 4) {
-                input.mi.dwFlags = MOUSEEVENTF_MIDDLEUP;
+            switch (button) {
+                case 1:
+                    input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+                    break;
+                case 2:
+                    input.mi.dwFlags = MOUSEEVENTF_RIGHTUP;
+                    break;
+                case 4:
+                    input.mi.dwFlags = MOUSEEVENTF_MIDDLEUP;
+                    break;
             }
             SendInput(1, &input, sizeof(INPUT));
 
             Sleep(delay);
 		}
     }
+
+    delete color;
 };
