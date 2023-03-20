bfb763c22125cbe7f8836ea4ce453b563d5556af
PyDev19 <atharvamishra956+1@gmail.com>
2023-03-20 14:46:14 +0530

    some refractoring

--- src/prompts/prompts.cpp
+++ src/prompts/prompts.cpp
@@ -4,11 +4,11 @@
 #include "../key/key.hpp"
 
 std::tuple<char, char, char> prompts(Key* key) {
-    char start_key = key -> get_key("Key to start autoclicker (press any key): ");
+    char start_key = key->get_key("Key to start autoclicker (press any key): ");
 
-    char stop_key = key -> get_key("Key to stop autoclicker (press any key): ");
+    char stop_key = key->get_key("Key to stop autoclicker (press any key): ");
 
-    char exit_key = key -> get_key("Key to exit program (press any key): ");
+    char exit_key = key->get_key("Key to exit program (press any key): ");
 
     return {start_key, stop_key, exit_key};
 }
