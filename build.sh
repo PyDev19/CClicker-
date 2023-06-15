6ae215ac8f4c1399f7b653bdccb95ded444b1b22
PyDev19 <atharvamishra956+1@gmail.com>
2023-06-14 20:42:08 -0500

    Used CMake and Make to build app

--- build.sh
+++ build.sh
@@ -1,20 +1,9 @@
-# Set variables for directories and files
-SRC_DIR=src
-INCLUDE_DIR=include
-BUILD_DIR=build
-COMPILED_DIR=$BUILD_DIR/obj
-APP_DIR=$BUILD_DIR/app
+# Build app using cmake and make
+mingw64-cmake .
+make .
 
-# Compile the cpp files in colors folder
-echo 'Compiling all c++ files...'
-x86_64-w64-mingw32-g++ -I$INCLUDE_DIR -c $SRC_DIR/colors.cpp -o $COMPILED_DIR/colors.o
-x86_64-w64-mingw32-g++ -I$INCLUDE_DIR -c $SRC_DIR/key.cpp -o $COMPILED_DIR/key.o
-x86_64-w64-mingw32-g++ -I$INCLUDE_DIR -c $SRC_DIR/clicker.cpp -o $COMPILED_DIR/clicker.o
-x86_64-w64-mingw32-g++ -I$INCLUDE_DIR -c $SRC_DIR/prompts.cpp -o $COMPILED_DIR/prompts.o
-x86_64-w64-mingw32-g++ -I$INCLUDE_DIR -c $SRC_DIR/main.cpp -o $COMPILED_DIR/main.o
-echo 'Done'
+# Move exe to build folder
+cp CClicker++.exe build/CClicker++.exe
 
-# Link all object files and resource file
-echo 'Linking all object and resource files...'
-x86_64-w64-mingw32-g++ $COMPILED_DIR/*.o icon.res -o $APP_DIR/CClicker++.exe
-echo 'Done'
+# Move lib dlls to build folder
+cp -r libs/* build/
