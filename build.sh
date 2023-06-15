b77c45b3980aecfa6c63ececf9ede8feffc33efd
PyDev19 <atharvamishra956+1@gmail.com>
2023-06-14 23:09:26 -0500

    made changes to cmake and sh to fix some things

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 # Build app using cmake and make
-mingw64-cmake .
-make .
+mingw64-cmake
+mingw64-make
 
 # Move exe to build folder
-cp CClicker++.exe build/CClicker++.exe
+mv CClicker++.exe build/CClicker++.exe
 
 # Move lib dlls to build folder
 cp -r libs/* build/
